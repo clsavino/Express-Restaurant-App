fe165c3eb1a5cfbae03f00777753d2d44d986bcf
Christi Savino <c.savino@ymail.com>
2016-10-14 12:52:40 -0400

    code cleanup

--- server.js
+++ server.js
@@ -19,43 +19,33 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// Basic route that sends the userto the landing page
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './app/public/landing.html'));
 });
-
+//route to display the reservation page - res.html)
 app.get('/reserve', function (req, res) {
     res.sendFile(path.join(__dirname, './app/public/res.html'));
 });
-
+// route to display the tables.html file
 app.get('/tables', function (req, res) {
     res.sendFile(path.join(__dirname, './app/public/tables.html'));
 });
-
+//api path to get the current reservations, responds with a json object (an array of reservations)
 app.get('/api/tables', function (req,res) {
-    console.log(req.params);
-    console.log('in server -.get/api/reservations, reservations', reservations);
     res.json(reservations);
 });
-
+// api path to get the current waitlist json object
 app.get('/api/waitlist', function(req,res) {
-    console.log(req.params);
-    console.log('in server -.get/api/waitlist, waitlist', waitlist);
     res.json(waitlist);
 });
 
-// *** Just updates an array? and sends back the json form of the new reservation??
+// *** Just updates an array of reservations and sends back the json form of the new reservation
 app.post('/api/tables', function (req, res) {
 	var newreservation = req.body;
 	newreservation.routeName = newreservation.customerName.replace(/\s+/g, '').toLowerCase();
-
-	console.log('\n in server.js, app.post("/api/tables")newreservation',newreservation);
-
     reservations.push(newreservation);
-
-    console.log('in server.js - reservations',reservations);
 	res.json(newreservation);
-
 });
 
 app.post('/api/waitlist', function (req, res) {
